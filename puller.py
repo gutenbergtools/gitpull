3df314fa6e01be3e1ad0d90a85a44b9d5e5db71b
Robert Tonsing <rt.dev@fastmail.com>
2026-06-10 12:38:53 -0500

    handle multiple trigger files, & file name correction

--- puller.py
+++ puller.py
@@ -50,11 +50,16 @@ def scan_dopull_log():
     Scan DOPULL_LOG_DIR for new files.
     Note: this does 3 things:
     1. For all trigger files, it pulls the latest files from the upstream repo into the FILES directory.
-    2. Copies .json files to JSON_LOG_DIR for database processing, and renames them as .info.txt trigger files.
-    3. Copies .zip.trig (all) files to DOPUSH_LOG_DIR for database updates.
+    2. Moves .json files to JSON_LOG_DIR for database processing, and creates an .info.txt trigger file.
+    3. Moves .zip.trig files to DOPUSH_LOG_DIR for database updates.
     Both directories are processed by FileInfo.py. In the future, it should be updated to do the appropriate
     processing for each file type, but for now this is a simple way to get the files where they need to go
     without needing to change FileInfo.py.
+
+    If both .zip.trig and .json files are present for the same ebook number:
+    (Workflow creates a .json, file, then Errata Workbench creates a .zip.trig file)
+    should be OK, the repo has all the changes, and we need the trigger file in any case.
+    Repo pull will occur twice, but the second will have no changes, snd this should be too rare to worry about.
     """
     for filename in sorted(os.listdir(DOPULL_LOG_DIR)):
         mode = os.stat(os.path.join(DOPULL_LOG_DIR, filename))[stat.ST_MODE]
@@ -65,10 +70,13 @@ def scan_dopull_log():
         ebook_num = 0
         m = re.match(r'^(\d+)\.(zip\.trig|json)$', filename)
         if m:
-            ebook_num = int(m.group(1))
+            ebook_num = m.group(1)
+            if not ebook_num.isdigit():
+                logging.error(f'Skipping invalid filename (non-numeric book number): {filename}')
+                continue
             logging.info(ebook_num)
             origin = f'{UPSTREAM_REPO_DIR}{ebook_num}.git/'
-            target_path = os.path.join(FILES, str(ebook_num))
+            target_path = os.path.join(FILES, ebook_num)
             logging.info(f'origin: {origin}, target_path: {target_path}')
 
             # Get the latest files from the upstream repo
@@ -76,25 +84,28 @@ def scan_dopull_log():
                 logging.error(f'failed to get files for {ebook_num}')
                 continue
 
+            # Now trigger database/catalog update
             try:
                 if filename.endswith('.json'):
-                    # For .json files, copy them to the JSON_LOG_DIR to add to the database
-                    shutil.copy(os.path.join(DOPULL_LOG_DIR, filename),
+                    # For .json files, move them to the JSON_LOG_DIR to add to the database
+                    shutil.move(os.path.join(DOPULL_LOG_DIR, filename),
                                  os.path.join(JSON_LOG_DIR, filename))
-                    logging.info(f'copied {filename} to JSON log directory for processing.')
-                    # Rename it as a trigger file
-                    newfilename = os.path.splitext(filename)[0] + '.info.txt'
-                    os.rename(os.path.join(DOPULL_LOG_DIR, filename),
-                            os.path.join(DOPULL_LOG_DIR, newfilename))
-                    filename = newfilename
+                    logging.info(f'moved {filename} to JSON log directory for processing.')
 
-                # Move all files to the DOPUSH_LOG_DIR to trigger updating
+                    # Create a corresponding .zip.trig trigger file
+                    trigger_file = os.path.join(DOPULL_LOG_DIR, ebook_num + '.zip.trig')
+                    if not os.path.exists(trigger_file):
+                        with open(trigger_file, 'w') as file:
+                            pass
+
+                # Move file to the DOPUSH_LOG_DIR to trigger updating
                 shutil.move(os.path.join(DOPULL_LOG_DIR, filename),
                                  os.path.join(DOPUSH_LOG_DIR, filename))
             except Exception as e:
                 logging.error(f'failed to trigger update for {ebook_num}: {e}')
 
     return
+
 
 def main():
     sys.exit(scan_dopull_log())
